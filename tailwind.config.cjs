3933362c6b4cc6a3028fd5000ebe79b4b4bc5cbb
augustCaio <caioaugusto930@gmail.com>
2023-08-11 11:37:39 -0300

    Initial commit: Setting up a mobile-first portfolio project using Astro framework, HTML, CSS, JavaScript, and Tailwind CSS

---
+++ tailwind.config.cjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			backgroundColor:{
+				primary: '#2B2C2C',
+				secondary:'#333333',
+				button: '#2D2E2E',
+				outline: '#C6C6C6'
+			},
+			textColor:{
+				primary: '#C6C6C6',
+				secondary: '#F8BB10',
+				third: '#777272',
+			},
+			ringColor:{
+				primary: '#C6C6C6',
+			},
+			fill:{
+				primary: '#C6C6C6'
+			},
+			fontFamily: {
+				'roboto': ['Roboto', 'sans-serif'],
+			},
+		},
+	},
+	plugins: ['prettier-plugin-tailwindcss'],
+}
